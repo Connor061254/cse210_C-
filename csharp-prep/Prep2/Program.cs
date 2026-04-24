ee6d5125c27b8bddb5f7baa2d4a8d5ff0dfa119d
Connor061254 <connor4148@gmail.com>
2026-04-24 15:56:52 -0700

    Second c# prep

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage?");
+        string gradePercentage = Console.ReadLine();
+
+        int strToInt = int.Parse(gradePercentage);
+
+        if (strToInt >= 90)
+        {
+            Console.Write("A");
+        }
+        else if (strToInt >= 80)
+        {
+            Console.Write("B");
+        }
+        else if(strToInt >= 70)
+        {
+            Console.Write("C");
+        }
+        else if (strToInt >= 60)
+        {
+            Console.Write("D");
+        }
+        else
+        {
+            Console.Write("F");
+        }
+
+
     }
 }
