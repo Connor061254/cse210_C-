3c79e86e3f722cce194f0aaddedc8fd98a0fe7e4
Connor061254 <connor4148@gmail.com>
2026-04-30 15:32:53 -0700

    worked on prep 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number? ");
+        string number = Console.ReadLine();
+        int int_number = int.Parse(number);
+        Console.WriteLine("What is your guess? ");
+        string response = Console.ReadLine();
+        int int_response = int.Parse(response);
+
+        while(int_response != int_number)
+        {
+            if (int_response < int_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("Lower");
+            }
+
+            Console.WriteLine("What is your guess? ");
+            string nextGuess = Console.ReadLine();
+            int intNextGuess= int.Parse(nextGuess);
+            int_response = intNextGuess;
+            
+        }
+
+        if(int_response == int_number)
+        {
+            Console.WriteLine("You Guessed it!");
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.Globalization;
+using System.Numerics;
+using System.Transactions;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        int input = 1;
+
+        while(input != 0)
+        {
+            Console.WriteLine("Enter a number: ");
+
+            if (int.TryParse(Console.ReadLine(), out input))
+            {
+               if(input != 0)
+                {
+                    numbers.Add(input);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please enter a number value");
+            }
+        }
+
+        int total = 0;
+        foreach(int num in numbers)
+        {
+            total += num;
+        }
+
+        Console.WriteLine($"You entered {total}");
+            
     }
 }
